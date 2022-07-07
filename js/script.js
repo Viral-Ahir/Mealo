828d66d4a0ad9f46a4b78536180c8434b6bd1e59
Aastha Savaliya <48683162+AasthaSavaliya@users.noreply.github.com>
2022-07-07 18:30:07 +0530

    ± contact info to mail

--- js/script.js
+++ js/script.js
@@ -5,13 +5,7 @@ const h1 = document.querySelector(".heading-primary");
 console.log(myName);
 console.log(h1);
 
-// h1.addEventListener("click", function () {
-//   h1.textContent = myName;
-//   h1.style.backgroundColor = "red";
-//   h1.style.padding = "5rem";
-// });
 
-///////////////////////////////////////////////////////////
 // Set current year
 const yearEl = document.querySelector(".year");
 const currentYear = new Date().getFullYear();
@@ -84,3 +78,12 @@ const obs = new IntersectionObserver(
 obs.observe(sectionHeroEl);
 
 // https://unpkg.com/smoothscroll-polyfill@0.4.4/dist/smoothscroll.min.js
+
+$(document).ready(function() {
+    $('#bt1').click(function() {
+        $('#fr1').attr('action',
+                       'mailto:aastha.savaliya01@gmail.com?subject=' +
+                       $('#tb1').val() + '&body=' + $('#tb2').val());
+        $('#fr1').submit();
+    });
+});
